71fb2e778adc3bc5c2690efa1dfdad0109ee2f1a
Svetlana Ovsiannikova <springtime070@mail.ru>
2022-12-14 15:07:15 +0200

    Добавляет правки на защите и немного тестов

---
+++ project/src/components/review-item/review-item.test.tsx
@@ -0,0 +1,24 @@
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import HistoryRouter from '../history-route/history-route';
+import {Provider} from 'react-redux';
+import {store} from '../../store';
+import {createMemoryHistory} from 'history';
+import ReviewItem from './review-item';
+import mockReview from '../../mocks/mock-review';
+
+describe('Component: PlaceCard', () => {
+  const history = createMemoryHistory();
+  const review = mockReview();
+
+  it('should render correctly', () => {
+    render(
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <ReviewItem review={review}/>
+        </HistoryRouter>
+      </Provider>);
+
+    expect(screen.getByText(new RegExp(`${review.comment}`))).toBeInTheDocument();
+  });
+});
